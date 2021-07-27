f7f053242c7d7433f3012d6fd82938fae260c7dd
Justin Huang <justin_huang@csu.fullerton.edu>
2021-07-26 18:37:48 -0700

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -23,38 +23,45 @@ var chartGroup = svg.append("g")
 
 
 
-d3.csv("data/clean_new_cars.csv").then(function(new_car_data){
+d3.csv("data/top_mpgs.csv").then(function(new_car_data){
   //Parse Data/Cast as number
     new_car_data.forEach(function(data){
       data.make = +data.make;
       data.model = +data.model;
       data.year = +data.year;
       data.popularity = +data.popularity;
+      data.highway_mpg = +data.highway_mpg;
+      data.city_mpg = +data.city_mpg;
+      data.msrp = +data.msrp;
+      data.level_0 = +data.level_0;
+
+
     });
   // Step 2: Create scale functions
-  var xLinearScale = d3.scaleLinear().domain([20,d3.max(new_car_data, d=> d.year)]).range([0,width]);
-  var yLinearScale = d3.scaleLinear().domain([0,d3.max(new_car_data,d => d.popularity)]).range([height,0]);
+  var xLinearScale = d3.scaleLinear().domain([0,d3.max(new_car_data, d=> d.city_mpg)]).range([0,width]);
+  var yLinearScale = d3.scaleLinear().domain([10,d3.max(new_car_data,d => d.highway_mpg)]).range([height,0]);
 
   // Step 3: Create axis functions
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft()
+
+  var bottomAxis = d3.axisBottom(xLinearScale)
+  var leftAxis = d3.axisLeft(yLinearScale);
 
   //Step 4: Append Axes to the charts
 
-  chartgGroup.append("g").attr("transform",`translate(0,${height})`).call(bottomAxis);
+  chartGroup.append("g").attr("transform",`translate(0,${height})`).call(bottomAxis);
 
   chartGroup.append("g").call(leftAxis);
 
 
   // Step 5: create circles
   var circlesGroup = chartGroup.selectAll("circle")
-    .data(hairData)
+    .data(new_car_data)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.year))
-    .attr("cy", d => yLinearScale(d.popularity))
+    .attr("cx", d => xLinearScale(d.city_mpg))
+    .attr("cy", d => yLinearScale(d.highway_mpg))
     .attr("r", "15")
-    .attr("fill", "pink")
+    .attr("fill", "blue")
     .attr("opacity", ".5");
 
     // Step 6: Initialize tool tip
@@ -62,7 +69,7 @@ d3.csv("data/clean_new_cars.csv").then(function(new_car_data){
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(d) {
-        return (`Make ${d.make}<br> Model: ${d.model} <br> Model Year : ${d.year} <br>Popularity: ${d.popularity}`);
+        return (`Make: ${d.make}  <br> Model: ${d.model}  <br> Model Year : ${d.year} <br> Popularity: ${d.popularity} <br> MSRP: ${d.msrp} <br> City MPG: ${d.city_mpg} <br>Highway MPG: ${d.highway_mpg}  `);
       });
     // Step 7: Create tooltip in the chart
     // ==============================
@@ -84,12 +91,12 @@ d3.csv("data/clean_new_cars.csv").then(function(new_car_data){
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .attr("class", "axisText")
-        .text("Popularity");
+        .text("Highway MPG");
 
       chartGroup.append("text")
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
         .attr("class", "axisText")
-        .text("Year)");
+        .text("City MPG");
     }).catch(function(error) {
       console.log(error);
     });
